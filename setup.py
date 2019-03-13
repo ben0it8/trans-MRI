a8475e0e301e0e7973d6c3c0627fa9039a073522
ben0it8 <oliver.atanaszov@gmail.com>
2019-03-13 17:51:33 +0100

    fixed setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
-setup(name='funniest',
+setup(name='trans_mri',
       version='0.1',
       description='Fast transfer learning experimentation with MRI data in PyTorch',
       url='http://github.com/ben0it8/trans_mri',
